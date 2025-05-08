027fcfc18be4975d0f049d91c9a4dd3037d4ff1b
aayushsaajtak <aayush.sharma@aajtak.com>
2025-05-08 12:48:41 +0530

    ignored canPlayFastForward check in setSpeed as it is unreliable

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -551,21 +551,23 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
         result([FlutterError errorWithCode:@"unsupported_speed"
                                    message:@"Speed must be >= 0.0 and <= 2.0"
                                    details:nil]);
-    } else if ((speed > 1.0 && _player.currentItem.canPlayFastForward) ||
-               (speed < 1.0 && _player.currentItem.canPlaySlowForward)) {
+    } 
+    // else if ((speed > 1.0 && _player.currentItem.canPlayFastForward) ||
+    //            (speed < 1.0 && _player.currentItem.canPlaySlowForward)) {
         _playerRate = speed;
         result(nil);
-    } else {
-        if (speed > 1.0) {
-            result([FlutterError errorWithCode:@"unsupported_fast_forward"
-                                       message:@"This video cannot be played fast forward"
-                                       details:nil]);
-        } else {
-            result([FlutterError errorWithCode:@"unsupported_slow_forward"
-                                       message:@"This video cannot be played slow forward"
-                                       details:nil]);
-        }
-    }
+    // } 
+    // else {
+    //     if (speed > 1.0) {
+    //         result([FlutterError errorWithCode:@"unsupported_fast_forward"
+    //                                    message:@"This video cannot be played fast forward"
+    //                                    details:nil]);
+    //     } else {
+    //         result([FlutterError errorWithCode:@"unsupported_slow_forward"
+    //                                    message:@"This video cannot be played slow forward"
+    //                                    details:nil]);
+    //     }
+    // }
 
     if (_isPlaying){
         _player.rate = _playerRate;
